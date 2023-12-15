732a1f415002b7f87d5e533d32772641ad3446eb
Papaabdoulaye <papaabdoulayepipo@gmail.com>
2023-12-15 17:34:37 +0000

    latest changes

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -3,3 +3,9 @@ import { generateReactHelpers } from "@uploadthing/react/hooks";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+
+import {generateComponents} from "@uploadthing/react"
+export const {
+    Uploader, 
+    UploadButton, 
+    UploadDropzone} = generateComponents<OurFileRouter>();
