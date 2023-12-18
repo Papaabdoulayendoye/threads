e46ee719c541194571b18e53a0e305f944e59aa7
Papaabdoulaye <papaabdoulayepipo@gmail.com>
2023-12-18 12:09:03 +0000

    latest changes

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -10,7 +10,9 @@ export const connectToDB = async () => {
     }
     if (isConnected) return console.log("Already connected to DB");
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL,{
+            dbName : "Threads",
+        });
         isConnected = true;
     } catch (error) {
         console.log("ERROR", error);
